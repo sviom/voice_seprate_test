1aa14ee45606569fc4bc8b25fd9f69dfb9db6d65
kanghanstar <kanghanstar@gmail.com>
2024-08-14 18:09:53 +0900

    수정: 무음 상태 판별 로직 및 예외 처리 추가
    무음 상태를 추적하여 시작 시간 누락 문제를 해결했습니다. 또한 예외 처리와 사운드 파일 삭제 기능을 추가하여 오류 발생 시 리소스 정리를 보장합니다.

--- main.py
+++ main.py
@@ -14,12 +14,15 @@ def detect_sound_intervals(file_path, interval_duration=0.05, threshold=0.002):
     sound_intervals = []
     no_slient_data = []
 
+    is_slient = True
     start_sec = 0
     end_sec = 0
 
     for start in range(0, len(y), interval_samples):
         end = start + interval_samples
-        start_sec += interval_duration
+
+        if is_slient:
+            start_sec += interval_duration
         end_sec += interval_duration
 
         # Get the current interval segment
@@ -31,28 +34,43 @@ def detect_sound_intervals(file_path, interval_duration=0.05, threshold=0.002):
         # Determine if the segment contains sound based on the threshold
         if rms >= threshold:
             no_slient_data.extend(segment)
+            is_slient = False
         else:
-            if len(no_slient_data) > 0:
+            if len(no_slient_data) > 0 and not is_slient:
                 sound_intervals.append(
                     {
                         "start_time": start_sec,
-                        "end_time": end / sr,
+                        "end_time": end_sec,
                         "segment": no_slient_data,
                         "sr": sr,
                     }
                 )
                 no_slient_data = []
+                is_slient = True
 
     return sound_intervals
 
 
-sound_intervals = detect_sound_intervals(file_path="audio.wav")
-for interval in sound_intervals:
-    start_sec = float(interval["start_time"])
-    end_sec = float(interval["end_time"])
-    segment = interval["segment"]
-    sr = interval["sr"]
+try:
+    sound_intervals = detect_sound_intervals(file_path="audio.wav")
+    for interval in sound_intervals:
+        start_sec = float(interval["start_time"])
+        end_sec = float(interval["end_time"])
+        segment = interval["segment"]
+        sr = interval["sr"]
 
-    if (end_sec - start_sec) > 0.05:
-        sf.write(f"sound_intervals_{str(start_sec)}.wav", data=segment, samplerate=sr)
-        print(f"Sound detected from {start_sec:.2f} to {end_sec:.2f} seconds")
+        if (end_sec - start_sec) >= 1:
+            sf.write(
+                f"sound_intervals_{str(start_sec)}.wav", data=segment, samplerate=sr
+            )
+            print(f"Sound detected from {start_sec:.2f} to {end_sec:.2f} seconds")
+
+except Exception as e:
+    print(e)
+finally:
+    import glob
+    import os
+
+    file_list = glob.glob("sound_intervals_*.wav")
+    for file_name in file_list:
+        os.remove(file_name)
