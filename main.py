03f8a92aad36c74ac7bd142d6b280acdee145513
kanghanstar <kanghanstar@gmail.com>
2024-08-14 17:33:32 +0900

    부드러운 구간 감지 및 저장 기능 추가
    사운드 구간을 감지하는 함수의 임계값을 수정하고, 감지된 구간을 개별 파일로 저장하는 기능을 추가했습니다. 이를 통해 무음 구간을 제외하고 사운드 데이터만 추출하여 파일로 저장할 수 있습니다.

--- main.py
+++ main.py
@@ -1,41 +1,57 @@
 import librosa
 import numpy as np
-import matplotlib.pyplot as plt
+import soundfile as sf
 
 
-def detect_sound_intervals(file_path, interval_duration=0.05, threshold=0.02):
+def detect_sound_intervals(file_path, interval_duration=0.05, threshold=0.002):
     # Load the audio file
     y, sr = librosa.load(file_path)
-
-    # plt.plot(y, label="data", alpha=0.7)
-    # plt.grid()
-    # plt.legend()
-    # plt.show()
 
     # Calculate the number of samples in each interval
     interval_samples = int(interval_duration * sr)
 
     # Initialize the list to store results
     sound_intervals = []
-
+    no_slient_data = []
+    start_time = float(0)
+    start_sec = 0
     # Iterate over the signal in intervals
     for start in range(0, len(y), interval_samples):
         end = start + interval_samples
+        start_sec += interval_duration
 
         # Get the current interval segment
         segment = y[start:end]
 
         # Calculate the root mean square (RMS) energy of the segment
         rms = np.sqrt(np.mean(segment**2))
-        print("rms : ", rms)
 
         # Determine if the segment contains sound based on the threshold
         if rms >= threshold:
-            sound_intervals.append((start / sr, end / sr))
+            no_slient_data.extend(segment)
+        else:
+            if len(no_slient_data) > 0:
+                sound_intervals.append(
+                    {
+                        "start_time": start_sec,
+                        "end_time": end / sr,
+                        "segment": no_slient_data,
+                        "sr": sr,
+                    }
+                )
+                no_slient_data = []
+                start_time = start_sec
 
     return sound_intervals
 
 
-detect_sound_intervals(file_path="audio.wav")
-# for interval in sound_intervals:
-#     print(f"Sound detected from {interval[0]:.2f} to {interval[1]:.2f} seconds")
+sound_intervals = detect_sound_intervals(file_path="audio.wav")
+for interval in sound_intervals:
+    start_sec = float(interval["start_time"])
+    end_sec = float(interval["end_time"])
+    segment = interval["segment"]
+    sr = interval["sr"]
+
+    if (end_sec - start_sec) > 0.05:
+        sf.write(f"sound_intervals_{str(start_sec)}.wav", data=segment, samplerate=sr)
+        print(f"Sound detected from {start_sec:.2f} to {end_sec:.2f} seconds")
