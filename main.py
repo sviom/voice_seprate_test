9536cdeff558e8635f525632e82b4d57bc4c50f6
kanghanstar <kanghanstar@gmail.com>
2024-08-14 18:43:19 +0900

    모듈화 및 파일 삭제
    main3.py 파일을 삭제하고, main.py의 기능을 sound_util.detect_sound 모듈로 이전했습니다. 코드의 가독성과 유지보수성을 위해 main.py의 기능을 sound_util.detect_sound 모듈로 분리하고 개선했습니다.

--- main.py
+++ main.py
@@ -1,70 +1,10 @@
-import librosa
-import numpy as np
 import soundfile as sf
 
-
-def detect_sound_intervals(file_path, interval_duration=0.05, threshold=0.005):
-    # Load the audio file
-    y, sr = librosa.load(file_path)
-
-    # Calculate the number of samples in each interval
-    interval_samples = int(interval_duration * sr)
-
-    # Initialize the list to store results
-    sound_intervals = []
-    no_slient_data = []
-
-    is_slient = True
-    start_sec = 0
-    end_sec = 0
-
-    for start in range(0, len(y), interval_samples):
-        end = start + interval_samples
-
-        if is_slient:
-            start_sec += interval_duration
-        end_sec += interval_duration
-
-        # Get the current interval segment
-        segment = y[start:end]
-
-        # Calculate the root mean square (RMS) energy of the segment
-        rms = np.sqrt(np.mean(segment**2))
-
-        # Determine if the segment contains sound based on the threshold
-        if rms >= threshold:
-            no_slient_data.extend(segment)
-            is_slient = False
-        else:
-            if len(no_slient_data) > 0 and not is_slient:
-                sound_intervals.append(
-                    {
-                        "start_time": start_sec,
-                        "end_time": end_sec,
-                        "segment": no_slient_data,
-                        "sr": sr,
-                    }
-                )
-                no_slient_data = []
-                is_slient = True
-
-    return sound_intervals
-
+from sound_util.detect_sound import detect_sound_intervals, create_sound_file
 
 try:
     sound_intervals = detect_sound_intervals(file_path="audio.wav")
-    for interval in sound_intervals:
-        start_sec = float(interval["start_time"])
-        end_sec = float(interval["end_time"])
-        segment = interval["segment"]
-        sr = interval["sr"]
-
-        if (end_sec - start_sec) >= 1:
-            sf.write(
-                f"sound_intervals_{start_sec:.2f}_{end_sec:.2f}.wav", data=segment, samplerate=sr
-            )
-            print(f"Sound detected from {start_sec:.2f} to {end_sec:.2f} seconds")
-
+    create_sound_file(sound_intervals)
 except Exception as e:
     print(e)
     import glob
