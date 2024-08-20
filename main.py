80e7d4adb13f24be9dad4f0c549c4c5e755c0a72
kanghanstar <kanghanstar@gmail.com>
2024-08-20 18:49:50 +0900

    추가: Google STT 기능 및 관련 의존성
    Google Cloud Speech-to-Text 기능을 추가하고 이를 main.py에서 호출하도록 수정했습니다. 또한, google-cloud-speech 패키지를 requirements.txt에 추가하고, 민감한 JSON 파일을 .gitignore에 포함시켰습니다.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from dotenv import load_dotenv
 from sound_util.detect_sound import detect_sound_intervals, create_sound_file
-from sound_util.stt_sound import create_speech_to_text
+from sound_util.stt_sound import create_speech_to_text, stt_using_google
 from sound_util.tts_sound import create_text_to_speech_file, tts_models
 
 try:
     load_dotenv()
+    stt_using_google(audio_file_path="audio.wav")
+
+
     sound_intervals = detect_sound_intervals(file_path="audio.wav")
     sound_file_list = create_sound_file(sound_intervals)
     text_list = create_speech_to_text(sound_file_list)
--- sound_util/stt_sound.py
+++ sound_util/stt_sound.py
@@ -14,3 +14,26 @@ def create_speech_to_text(audio_file_list: list[str]):
             text_list.append(transcription.text)
 
     return text_list
+
+
+def stt_using_google(audio_file_path: str):
+    import speech_recognition as sr
+
+    try:
+        r = sr.Recognizer()
+        with sr.AudioFile(audio_file_path) as source:
+            audio = r.record(source)  # read the entire audio file
+
+        response = r.recognize_google_cloud(
+            audio_data=audio,
+            credentials_json="google_credential.json",
+            language="ko-KR",
+        )
+        print("successful: ", response)
+    except sr.UnknownValueError:
+        print("Google Cloud Speech could not understand audio")
+
+    except sr.RequestError as e:
+        print(
+            "Could not request results from Google Cloud Speech service; {0}".format(e)
+        )
